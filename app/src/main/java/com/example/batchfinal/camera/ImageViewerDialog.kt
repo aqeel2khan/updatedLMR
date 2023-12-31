ebebe38625825dc16f107d2e56c84b9c243bf84c
ansaralisahh <iansarsah@gmail.com>
2024-01-01 00:33:54 +0530

    change something

--- app/src/main/java/com/example/batchfinal/camera/ImageViewerDialog.kt
+++ app/src/main/java/com/example/batchfinal/camera/ImageViewerDialog.kt
@@ -5,7 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.DialogFragment
-import com.example.ajzoproject.R
+import com.example.batchfinal.R
 
 /**
  * Dialog to View Image
