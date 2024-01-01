7f187e7cdbf8942dcc87cec9473775e226b84d31
Hp <aqeel2.er.delhi@gmail.com>
2024-01-01 18:01:27 +0300

    API call from Basic Detail

--- app/src/main/java/com/example/batchfinal/BasicDetailsActivity.kt
+++ app/src/main/java/com/example/batchfinal/BasicDetailsActivity.kt
@@ -7,45 +7,96 @@ import android.widget.ArrayAdapter
 import android.widget.EditText
 import android.widget.Spinner
 import android.widget.TextView
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import com.example.batchfinal.R
 import com.example.batchfinal.databinding.ActivityBasicDetailsBinding
+import com.example.batchfinal.view.BaseActivity
+import com.example.batchfinal.viewmodel.AllViewModel
+import com.example.batchfinal.viewmodel.BaseViewModel
+import com.example.batchfinal.viewmodel.BasicDetailViewModel
+import com.google.gson.JsonObject
+import dagger.hilt.android.AndroidEntryPoint
+import org.json.JSONObject
 
+@AndroidEntryPoint
+class BasicDetailsActivity : BaseActivity<ActivityBasicDetailsBinding>() {
+    private val viewModel: BasicDetailViewModel by viewModels()
 
-class BasicDetailsActivity : AppCompatActivity() {
-    private lateinit var mBinding: ActivityBasicDetailsBinding
+//    private lateinit var mBinding: ActivityBasicDetailsBinding
     private var isPrivateEvent = true
     private var isPublicEvent = true
     private var isFreeEvent = true
     private var isPaidEvent = true
-    @SuppressLint("MissingInflatedId")
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_basic_details)
-        mBinding = ActivityBasicDetailsBinding.inflate(layoutInflater)
-        setContentView(mBinding.root)
+//    @SuppressLint("MissingInflatedId")
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        setContentView(R.layout.activity_basic_details)
+//        mBinding = ActivityBasicDetailsBinding.inflate(layoutInflater)
+//        setContentView(mBinding.root)
+//
+//        val toolbar: Toolbar = findViewById(R.id.toolbar)
+//        setSupportActionBar(toolbar)
+//        supportActionBar!!.setDisplayShowTitleEnabled(false)
+//
+//        mBinding.publicEvent.setOnClickListener {
+//            isPrivateEvent = !isPrivateEvent
+//            mBinding.publicEvent.isChecked = isPrivateEvent
+//        }
+//
+//        mBinding.privateEvent.setOnClickListener {
+//            isPublicEvent = !isPublicEvent
+//            mBinding.privateEvent.isChecked = isPublicEvent
+//        }
+//        mBinding.freeEvent.setOnClickListener {
+//            isFreeEvent = !isFreeEvent
+//            mBinding.freeEvent.isChecked = isFreeEvent
+//        }
+//        mBinding.paidEvent.setOnClickListener {
+//            isPaidEvent = !isPaidEvent
+//            mBinding.paidEvent.isChecked = isPaidEvent
+//        }
+//
+//        customSpinnerChooseEventCategory();
+//        customSpinnerEvent();
+//        customSpinnerTotalMaximumCapacity();
+//        customSpinnerAgeGroup();
+//
+//        mBinding.saveAndContinueButtonBasic.setOnClickListener {
+//            startActivity(Intent(this, EventDescriptionActivity::class.java))
+//        }
+//    }
 
-        val toolbar: Toolbar = findViewById(R.id.toolbar)
+    override fun getViewModel(): BaseViewModel {
+        return viewModel
+
+    }
+    override fun getViewBinding() = ActivityBasicDetailsBinding.inflate(layoutInflater)
+
+
+    override fun initUi() {
+
+                val toolbar: Toolbar = findViewById(R.id.toolbar)
         setSupportActionBar(toolbar)
         supportActionBar!!.setDisplayShowTitleEnabled(false)
 
-        mBinding.publicEvent.setOnClickListener {
+        binding.publicEvent.setOnClickListener {
             isPrivateEvent = !isPrivateEvent
-            mBinding.publicEvent.isChecked = isPrivateEvent
+            binding.publicEvent.isChecked = isPrivateEvent
         }
 
-        mBinding.privateEvent.setOnClickListener {
+        binding.privateEvent.setOnClickListener {
             isPublicEvent = !isPublicEvent
-            mBinding.privateEvent.isChecked = isPublicEvent
+            binding.privateEvent.isChecked = isPublicEvent
         }
-        mBinding.freeEvent.setOnClickListener {
+        binding.freeEvent.setOnClickListener {
             isFreeEvent = !isFreeEvent
-            mBinding.freeEvent.isChecked = isFreeEvent
+            binding.freeEvent.isChecked = isFreeEvent
         }
-        mBinding.paidEvent.setOnClickListener {
+        binding.paidEvent.setOnClickListener {
             isPaidEvent = !isPaidEvent
-            mBinding.paidEvent.isChecked = isPaidEvent
+            binding.paidEvent.isChecked = isPaidEvent
         }
 
         customSpinnerChooseEventCategory();
@@ -53,37 +104,42 @@ class BasicDetailsActivity : AppCompatActivity() {
         customSpinnerTotalMaximumCapacity();
         customSpinnerAgeGroup();
 
-        mBinding.saveAndContinueButtonBasic.setOnClickListener {
+        binding.saveAndContinueButtonBasic.setOnClickListener {
             startActivity(Intent(this, EventDescriptionActivity::class.java))
         }
+        //Todo Call API
+
+        viewModel.callPostEvent(JsonObject())
     }
+
+
 
 
     private fun customSpinnerEvent() {
         val items =
             arrayOf("Marriage 1", "Party 2", "Birthday  3", "Anniversary 4", "Rewards Party 5")
         val adapter = ArrayAdapter(this, R.layout.custom_dropdown_item, items)
-        mBinding.customSpinner1.adapter = adapter
+        binding.customSpinner1.adapter = adapter
     }
 
     private fun customSpinnerChooseEventCategory() {
         val items =
             arrayOf("Dawat Waleema1", "Reception ", "Price Reward", "Annual Party", "Eid Party")
         val adapter = ArrayAdapter(this, R.layout.custom_dropdown_item, items)
-        mBinding.customSpinner2.adapter = adapter
+        binding.customSpinner2.adapter = adapter
     }
 
     private fun customSpinnerTotalMaximumCapacity() {
         val items = arrayOf("100-200", "200-300 ", "300-400", "400-500")
         val adapter = ArrayAdapter(this, R.layout.custom_dropdown_item, items)
-        mBinding.customSpinner3.adapter = adapter
+        binding.customSpinner3.adapter = adapter
     }
 
 
     private fun customSpinnerAgeGroup() {
         val items = arrayOf("18", "19 ", "20", "21")
         val adapter = ArrayAdapter(this, R.layout.custom_dropdown_item, items)
-        mBinding.customSpinner4.adapter = adapter
+        binding.customSpinner4.adapter = adapter
     }
 
 
